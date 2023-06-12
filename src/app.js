f21f23d64d3a0cf2f9b9e68229b83c950d0cfaaf
Catarinals <132306244+Catarinals@users.noreply.github.com>
2023-06-12 18:51:19 +0100

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response);
 
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "ea011et20fa37354b09b96e56b43o2f3";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=ea011et20fa37354b09b96e56b43o2f3&units=metric";
+axios.get(apiUrl).then(displayTemperature);
