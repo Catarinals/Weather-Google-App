98f8b8c0a8801332723d4efe292324a46142c8fb
Catarinals <132306244+Catarinals@users.noreply.github.com>
2023-06-12 19:29:46 +0100

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
+{function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10){
+    hours = `0${hours}`;
+let minutes = date.getMinutes();
+if (minutes < 10){
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response);
+  console.log(response.data.time);
 
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -11,8 +25,12 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formateDate(response.data.time * 1000);
 }
+
 let apiKey = "ea011et20fa37354b09b96e56b43o2f3";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=ea011et20fa37354b09b96e56b43o2f3&units=metric";
+
 axios.get(apiUrl).then(displayTemperature);
