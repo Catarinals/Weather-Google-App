f38fd1feee82013f7802fb63121b1540c5a864f6
Catarinals <132306244+Catarinals@users.noreply.github.com>
2023-06-13 15:08:27 +0100

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -1,20 +1,27 @@
-{function formatDate(timestamp){
-let date = new Date(timestamp);
-let hours = date.getHours();
-if (hours < 10){
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
     hours = `0${hours}`;
-let minutes = date.getMinutes();
-if (minutes < 10){
-    minutes = `0${minutes}`;
-}
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day = days[date.getDay()];
-return `${day} ${hours}:${minutes}`;
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+  }
 }
 
 function displayTemperature(response) {
-  console.log(response.data.time);
-
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#city");
@@ -26,11 +33,17 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formateDate(response.data.time * 1000);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "ea011et20fa37354b09b96e56b43o2f3";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=ea011et20fa37354b09b96e56b43o2f3&units=metric";
+let city = "Lisbon";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=ea011et20fa37354b09b96e56b43o2f3&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
